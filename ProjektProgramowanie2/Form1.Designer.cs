9178e20904e2e449867150e266fc80d4201e8d08
Szymon Janecki <szjanecki@gmail.com>
2020-02-11 09:59:35 +0100

    Add files via upload

---
+++ ProjektProgramowanie2/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿using System.ComponentModel;
+
+namespace ProjektProgramowanie2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.rawRadioButton = new System.Windows.Forms.RadioButton();
+            this.parsedRadioButton = new System.Windows.Forms.RadioButton();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(90, 32);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(243, 20);
+            this.searchBox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(367, 32);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(130, 24);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // rawRadioButton
+            // 
+            this.rawRadioButton.AutoSize = true;
+            this.rawRadioButton.Location = new System.Drawing.Point(120, 58);
+            this.rawRadioButton.Name = "rawRadioButton";
+            this.rawRadioButton.Size = new System.Drawing.Size(47, 17);
+            this.rawRadioButton.TabIndex = 2;
+            this.rawRadioButton.Text = "Raw";
+            this.rawRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // parsedRadioButton
+            // 
+            this.parsedRadioButton.AutoSize = true;
+            this.parsedRadioButton.Checked = true;
+            this.parsedRadioButton.Location = new System.Drawing.Point(196, 58);
+            this.parsedRadioButton.Name = "parsedRadioButton";
+            this.parsedRadioButton.Size = new System.Drawing.Size(58, 17);
+            this.parsedRadioButton.TabIndex = 3;
+            this.parsedRadioButton.TabStop = true;
+            this.parsedRadioButton.Text = "Parsed";
+            this.parsedRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(69, 101);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(759, 360);
+            this.outputBox.TabIndex = 4;
+            this.outputBox.Text = "";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(530, 32);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(280, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(867, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 491);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.parsedRadioButton);
+            this.Controls.Add(this.rawRadioButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.RadioButton rawRadioButton;
+        private System.Windows.Forms.RadioButton parsedRadioButton;
+        private System.Windows.Forms.RichTextBox outputBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+
+}
+
---
+++ ProjektProgramowanie2/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace ProjektProgramowanie2
+{
+    public partial class Form1 : Form
+    {
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* 
+         *  Metoda jest wywoływana w momencie gdy zostanie naciśnięty Button1
+         *  Sprawdza czy backgroundWorker1 jest zajęty i
+         *  jeżeli jest wolny, uruchamia go i przekazuje jako argument
+         *  tekst z searchBoxa.
+         */
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync(this.searchBox.Text);
+            }
+        }
+
+        /* Metoda odpowiadająca za pracę backgroundWorkera
+         * Wywoływana zostaje po tym jak zostanie wywołany kod
+         * "backgroundWorker1.RunWorkerAsync(this.searchBox.Text);"
+         * Pobiera jako argument nazwę miasta i tworzy przy jego pomocy
+         * URI które zostaje użyte do połączenia się z API OpenWeatherMap
+         * Następnie w sposób asynchroniczny pobiera z niego dane o pogodzie z danego miasta
+         * używając do tego biblioteki HttpClient
+         * Na koniec przekazuje wynik do e.Result
+         * Dodatkowo po każdej ważniejszej operacji wywoływana jest metoda ReportProgress
+         * aby dać ogólną informację na jakim etapie wykonania jest nasz wątek
+         */
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            worker.ReportProgress(0);
+
+            string baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+            string city = e.Argument.ToString();
+            string apiKey = "INSERT YOUR KEY HERE";
+
+            worker.ReportProgress(10);
+
+            string url = baseUrl + "?q=" + city + "&appid=" + apiKey;
+            Uri uri = new Uri(url);
+
+            worker.ReportProgress(30);
+
+            var requestCode = client.GetAsync(uri).Result.StatusCode;
+
+            worker.ReportProgress(50);
+
+            if (requestCode.ToString() == "OK")
+            {
+                e.Result = client.GetStringAsync(new Uri(url)).Result.ToString();
+            }
+            else
+            {
+                e.Result = "Wartość pola search jest niepoprawna";
+            }
+            worker.ReportProgress(100);
+        }
+
+        /*
+         * Metoda ProgressChanged wykonuje się za każdym razem, gdy 
+         * w DoWork wywoływane jest ReportProgress
+         * Służy ona do aktualizacji progressBar1
+         * */
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        /*RunWorkerCompleted wykonuje się w momencie gdy DoWork skończy pracę.
+         * Pobiera ona e.Result i konwertuje jego zawartość do klasy String
+         * Następnie sprawdza czy chcemy dane w postaci surowego JSONa
+         * czy chcemy je w postaci sformatowanej.
+         * W pierwszym przypadku po prostu przekazujemy result do outputBox.Text
+         * W drugim używamy biblioteki Json.NET do sparsowania naszego JSONa
+         * następnie korzystając z metod tej biblioteki wyciągamy z JSONa kilka 
+         * przykładowych informacji i przekazujemy je do outputBox.Text
+         * */
+
+        private void backgroundWorker1_RunWorkerCompleted(
+           object sender, RunWorkerCompletedEventArgs e)
+        {
+            string result = e.Result.ToString();
+
+            if(result.Equals("Wartość pola search jest niepoprawna") || rawRadioButton.Checked)
+            {
+                this.outputBox.Text = e.Result.ToString();
+            }
+            else
+            {
+                JObject weather = JObject.Parse(result);
+
+                JObject main = weather.Value<JObject>("main");
+
+                String city = weather.Value<String>("name");
+
+                double temp = main.Value<double>("temp") - 273.15; //API podaje temperaturę w Kelvinach
+                String pressure = main.Value<String>("pressure");
+
+                this.outputBox.Text = "City: " + city + "\n"
+                    + "Temperature: " + temp + " C \n"
+                    + "Pressure: " + pressure + "\n";
+            }
+
+        }
+
+        /*
+         * Zamyka formatkę po wybraniu opcji Exit z MenuStrip
+         * */
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
